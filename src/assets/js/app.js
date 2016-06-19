7e3bbb1ae5c934e09ff24625fd1fc4b52a75f063
Jneiss <nais@gmx.at>
2016-06-19 20:06:37 +0200

    nigger

---
+++ src/assets/js/app.js
@@ -0,0 +1,45 @@
+
+
+
+$(document).foundation();
+
+$(document).ready(function(){
+  $('.header-slideshow').slick({
+ //  autoplay: true,
+   autoplaySpeed: 3000,
+
+  });
+
+
+$('#nav').onePageNav();
+
+});
+
+$('.top-bar-title').on('click','a',function(){
+	$('#nav').children().removeClass('current');
+});
+
+
+
+
+function ready() {
+    var box = document.getElementById('sticky-bar');
+    var top = $(box).offset().top;
+    function scroll(event) {
+        var y = document['documentElement' || 'body'].scrollTop;
+
+        if (y >= top) box.classList.add('shrink');
+        else box.classList.remove('shrink');
+
+    }
+
+    window.addEventListener('scroll', scroll);
+}
+
+if (document.readyState == 'complete' || document.readyState == 'loaded') {
+    ready();
+} else {
+    window.addEventListener('DOMContentLoaded', ready);
+};
+
+
